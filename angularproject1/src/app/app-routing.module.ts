8118822d2da90dd4d38d2bbae67060d04bc3b5d0
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-07-10 12:12:38 +0600

    angularproject(10/07/24)
    Upadate till delete mathod (12:12PM)

---
+++ angularproject1/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+
+const routes: Routes = [
+  {path:"student", component:StudentComponent},
+  {path:"employee", component:EmployeeComponent},
+  {path:"location", component:LocationComponent},
+  {path:"createLocation", component:CreatelocationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
