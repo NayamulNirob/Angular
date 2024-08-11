8c9b0840ce6ad07abe34d0d75ac13a710e996c5c
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-08-11 12:42:43 +0600

    SportsStore 11/08/24
    12:42PM

---
+++ sportsstore/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'sportsstore';
+  icon=faCoffee;
+  
+}
