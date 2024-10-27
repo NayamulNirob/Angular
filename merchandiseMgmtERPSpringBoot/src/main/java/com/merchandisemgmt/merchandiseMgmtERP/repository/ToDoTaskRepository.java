f70aa2d6a014f7392fb1124d85596d81ce82beb1
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-10-28 01:34:39 +0600

    deleted
    1:34PM
    28/10/24

--- merchandiseMgmtERPSpringBoot/src/main/java/com/merchandisemgmt/merchandiseMgmtERP/repository/ToDoTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.merchandisemgmt.merchandiseMgmtERP.repository;
-
-import com.merchandisemgmt.merchandiseMgmtERP.entity.ToDoTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoTaskRepository extends JpaRepository<ToDoTask,Long > {
-
-}
