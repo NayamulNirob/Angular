1574517c5494a01fd6e5472bdc30d234aab15f1a
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2025-03-12 03:08:18 +0600

    product_practice

--- angularpractice/src/app/app.component.ts
+++ angularpractice/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavbarComponent, SidebarComponent]
   
 })
 export class AppComponent {
---
+++ product_practice_angular/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './design/navbar/navbar.component';
+
+const routes: Routes = [
+  {path:"navbar",component:NavbarComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
